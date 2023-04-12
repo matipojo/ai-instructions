ab0c68281acce3eb2bc2ec30c8fd9903e5b26019
Mati Horovitz <7645314@gmail.com>
2023-04-12 23:51:55 +0300

    Fix: App

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { TodoApp } from './components/TodoApp';
+import { App } from './components/App';
 
 ReactDOM.render(
-    <TodoApp />,
+    <App />,
     document.getElementById('root')
 );
